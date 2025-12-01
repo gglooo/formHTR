3c74e3c4591c6beca2bfd0bc2c11291f87e7e824
Jan Glos <glos2001@seznam.cz>
2025-12-01 13:11:44 +0100

    feat: enable headless select_ROIs via args

--- select_ROIs.py
+++ select_ROIs.py
@@ -10,7 +10,7 @@ from libs.pdf_to_image import convert_pdf_to_image, resize_image
 from libs.region import ROI
 
 
-def main(filename, autodetect, autodetect_filter, output_file, config_file, detect_residuals, credentials, display_residuals):
+def main(filename, autodetect, autodetect_filter, output_file, config_file, detect_residuals, credentials, display_residuals, headless):
     """Convert given PDF to image, automatically detect rectangles (ROIs)
     and then allow to draw rectagles manually. Coordinates of all
     ROIs are then exported to a CSV file.
@@ -45,7 +45,17 @@ def main(filename, autodetect, autodetect_filter, output_file, config_file, dete
         height, width, _ = image.shape
         config = LogsheetConfig(rectangles, residuals, height, width)
 
-    config.export_to_json(output_file)
+    if headless:
+        config.export_to_json(output_file)
+        return
+
+    ROIs_widget = SelectROIsWidget(image, config, display_residuals)
+
+    process_cli(ROIs_widget)
+
+    cv2.destroyAllWindows()
+    ROIs_widget.config.export_to_json(output_file)
+
 
 
 if __name__ == '__main__':
@@ -64,10 +74,14 @@ if __name__ == '__main__':
     optional.add_argument('--detect_residuals', action=argparse.BooleanOptionalAction, default=False, help='Find existing text in the template and flag it as residual (will be always ignored)')
     required.add_argument('--credentials', type=str, help='Path to Google vision credentials')
     optional.add_argument('--display_residuals', action=argparse.BooleanOptionalAction, default=False, help='Display found residuals.')
+    optional.add_argument('--headless', action=argparse.BooleanOptionalAction, default=False, help='Run in headless mode without GUI interaction.')
 
     args = args_parser.parse_args()
     
     if args.detect_residuals and not args.credentials:
         args_parser.error('The --detect_residuals argument requires --credentials.')
+    
+    if args.headless and args.display_residuals:
+        args_parser.error('The --headless argument cannot be used together with --display_residuals.')
 
-    main(args.pdf_file, args.autodetect, args.autodetect_filter, args.output_file, args.config_file, args.detect_residuals, args.credentials, args.display_residuals)
+    main(args.pdf_file, args.autodetect, args.autodetect_filter, args.output_file, args.config_file, args.detect_residuals, args.credentials, args.display_residuals, args.headless)
